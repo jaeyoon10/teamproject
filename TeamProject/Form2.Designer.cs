bdf001b687508030ad873193b9bad48142355170
geonhee5593 <hgh717171@gmail.com>
2024-11-14 12:49:37 +0900

    로그인 페이지,상품 등록 페이지 만드는중

--- TeamProject/Form2.Designer.cs
+++ TeamProject/Form2.Designer.cs
@@ -28,12 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Pregister_text = new System.Windows.Forms.Label();
+            this.Pmanage_text = new System.Windows.Forms.Label();
+            this.Sale_text = new System.Windows.Forms.Label();
+            this.Report_text = new System.Windows.Forms.Label();
+            this.data_register = new System.Windows.Forms.DataGridView();
+            this.Logout = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.data_register)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pregister_text
+            // 
+            this.Pregister_text.AutoSize = true;
+            this.Pregister_text.Font = new System.Drawing.Font("궁서체", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Pregister_text.Location = new System.Drawing.Point(34, 142);
+            this.Pregister_text.Name = "Pregister_text";
+            this.Pregister_text.Size = new System.Drawing.Size(148, 30);
+            this.Pregister_text.TabIndex = 0;
+            this.Pregister_text.Text = "상품 등록";
+            this.Pregister_text.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Pmanage_text
+            // 
+            this.Pmanage_text.AutoSize = true;
+            this.Pmanage_text.Font = new System.Drawing.Font("궁서체", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Pmanage_text.Location = new System.Drawing.Point(34, 308);
+            this.Pmanage_text.Name = "Pmanage_text";
+            this.Pmanage_text.Size = new System.Drawing.Size(148, 30);
+            this.Pmanage_text.TabIndex = 1;
+            this.Pmanage_text.Text = "상품 관리";
+            // 
+            // Sale_text
+            // 
+            this.Sale_text.AutoSize = true;
+            this.Sale_text.Font = new System.Drawing.Font("궁서체", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Sale_text.Location = new System.Drawing.Point(34, 477);
+            this.Sale_text.Name = "Sale_text";
+            this.Sale_text.Size = new System.Drawing.Size(148, 30);
+            this.Sale_text.TabIndex = 2;
+            this.Sale_text.Text = "판매 내역";
+            // 
+            // Report_text
+            // 
+            this.Report_text.AutoSize = true;
+            this.Report_text.Font = new System.Drawing.Font("궁서체", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Report_text.Location = new System.Drawing.Point(21, 646);
+            this.Report_text.Name = "Report_text";
+            this.Report_text.Size = new System.Drawing.Size(178, 30);
+            this.Report_text.TabIndex = 3;
+            this.Report_text.Text = "보고서 관리";
+            // 
+            // data_register
+            // 
+            this.data_register.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_register.Location = new System.Drawing.Point(413, 210);
+            this.data_register.Name = "data_register";
+            this.data_register.RowHeadersWidth = 62;
+            this.data_register.RowTemplate.Height = 30;
+            this.data_register.Size = new System.Drawing.Size(1201, 636);
+            this.data_register.TabIndex = 4;
+            // 
+            // Logout
+            // 
+            this.Logout.AutoSize = true;
+            this.Logout.Font = new System.Drawing.Font("궁서체", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Logout.Location = new System.Drawing.Point(47, 881);
+            this.Logout.Name = "Logout";
+            this.Logout.Size = new System.Drawing.Size(133, 30);
+            this.Logout.TabIndex = 5;
+            this.Logout.Text = "로그아웃";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1484, 879);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 47);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "상품 등록";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1262, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 47);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "상품 조회";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("궁서체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(410, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "등록 날짜";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(410, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "등록 가격";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(650, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "카테고리";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(659, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "비고";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1778, 944);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Logout);
+            this.Controls.Add(this.data_register);
+            this.Controls.Add(this.Report_text);
+            this.Controls.Add(this.Sale_text);
+            this.Controls.Add(this.Pmanage_text);
+            this.Controls.Add(this.Pregister_text);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data_register)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Pregister_text;
+        private System.Windows.Forms.Label Pmanage_text;
+        private System.Windows.Forms.Label Sale_text;
+        private System.Windows.Forms.Label Report_text;
+        private System.Windows.Forms.DataGridView data_register;
+        private System.Windows.Forms.Label Logout;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
