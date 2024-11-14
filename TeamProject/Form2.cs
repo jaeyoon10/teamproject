ca71d3e7a301c382dfa02b2eb730c43be105bb4d
geonhee5593 <hgh717171@gmail.com>
2024-11-14 13:58:19 +0900

    폼 2 만듬

--- TeamProject/Form2.cs
+++ TeamProject/Form2.cs
@@ -36,5 +36,20 @@ namespace TeamProject
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
